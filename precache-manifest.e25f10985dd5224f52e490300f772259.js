ea8a44c3c72acfa4def1e7a32c4ff6ab70befcca
Andrii Yozh <Andriiyozh@Volodymyrs-MacBook-Pro-3.local>
2020-11-03 20:57:11 +0200

    Updates

---
+++ precache-manifest.e25f10985dd5224f52e490300f772259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3923135c1124eaa4c0f0c8ac6a21d108",
+    "url": "/my-todolist/index.html"
+  },
+  {
+    "revision": "b947e31fb56b36cf927c",
+    "url": "/my-todolist/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "c888a39dff7a208c6d8e",
+    "url": "/my-todolist/static/css/main.6c54fb73.chunk.css"
+  },
+  {
+    "revision": "b947e31fb56b36cf927c",
+    "url": "/my-todolist/static/js/2.786939eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-todolist/static/js/2.786939eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c888a39dff7a208c6d8e",
+    "url": "/my-todolist/static/js/main.f67513b5.chunk.js"
+  },
+  {
+    "revision": "2d0d43a54008ba8f8e1e",
+    "url": "/my-todolist/static/js/runtime-main.0bbd18fd.js"
+  }
+]);
